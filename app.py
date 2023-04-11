76b74da7c1c6218c04139c7968cb7732178133b0
Aakshar-D <70840216+Aakshar-D@users.noreply.github.com>
2023-04-11 09:11:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,8 @@ def upload():
             return render_template('Home.html', error='Unsupported file format. Please upload a CSV or Excel file.')
 
         df.to_csv('csv_templates\data.csv', index=False)
-        subprocess.call('python3','Demo_sych.py', shell=True)
+        subprocess.call(['python3','Demo_sych.py'], shell=True)
+        subprocess.Popen(['python3', 'Demo_sych.py'], bufsize=0)
 
         return render_template('home.html', success='File uploaded successfully.')
     return render_template('Home.html')
